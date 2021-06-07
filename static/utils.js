502b542eaeedfe15bef2916a29cad8fe66835427
Manuel Vinent Guilarte <mjavint@gmail.com>
2021-06-07 08:09:57 -0400

    Completo

---
+++ static/utils.js
@@ -0,0 +1,58 @@
+function loadDoc(e) {
+    const file = e.target.files[0];
+  
+    if (!file) {
+      throw new Error('You need to choose an XML file first')
+      alert('You need to choose an XML file first')
+      return false
+    }
+  
+    readDoc(file).then(parseDoc).then(showDocInTable).catch(onError)
+  }
+  
+  function readDoc(file) {
+    const reader = new FileReader()
+  
+    return new Promise((ok) => {
+      reader.readAsText(file)
+      reader.onload = function() {
+        ok(reader.result)
+      }
+    })
+  }
+  
+  function parseDoc(rawXML) {
+    const parser = new DOMParser()
+    const xml = parser.parseFromString(rawXML, 'text/html')
+    return xml
+  }
+  
+  function showDocInTable(xml) {
+    const table = document.querySelector('#bookTable > tbody')
+    const datasource = xml.querySelector('bookstore')
+    const books = datasource.querySelectorAll('book')
+  
+    table.removeChild(table.children[0])
+  
+    Array.from(books).map((book, i) => {
+      const tr = document.createElement('tr')
+      const id = tagToData(book.querySelector('ID'))
+      const agencia = tagToData(book.querySelector('Agencia'))
+      const ag_origen = tagToData(book.querySelector('AG_Origen'))
+      const dpa = tagToData(book.querySelector('DPA'))
+      const dpa_origen = tagToData(book.querySelector('DPA_Origen'))
+      const fecha_ec = tagToData(book.querySelector('Fecha_EC'))
+      const fecha_pago = tagToData(book.querySelector('Fecha_Pago'))
+      const importe = tagToData(book.querySelector('Importe'))
+      const referencia = tagToData(book.querySelector('Referencia'))
+      const Referencia_Origen = tagToData(book.querySelector('Referencia_Origen'))
+      const codigo_barra = tagToData(book.querySelector('Codigo_Barra'))
+      const nit = tagToData(book.querySelector('NIT'))
+      const impuesto = tagToData(book.querySelector('Impuesto'))
+      const periodo = tagToData(book.querySelector('Periodo'))
+      const contribuyente = tagToData(book.querySelector('Contribuyente'))
+  
+      tr.append(id, agencia, ag_origen, dpa, dpa_origen, fecha_ec, fecha_pago, importe, referencia, Referencia_Origen, codigo_barra, nit, impuesto, periodo, contribuyente
+      table.appendChild(tr)
+    })
+  }
